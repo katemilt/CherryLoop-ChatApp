527ca67cc6b18501f40b8fb2539b220aa841737a
katemilt <kaitlynmiltimore@gmail.com>
2024-10-15 15:03:32 -0400

    Adding starting files

---
+++ cherry-loop/src/App.jsx
@@ -0,0 +1,12 @@
+import Login from './sections/Login.jsx';
+
+const App = () => {
+  return (
+    <main className="max-w-7xl mx-auto relative">
+      <p>Hello</p>
+      <Login />
+    </main>
+  );
+};
+
+export default App;
---
+++ cherry-loop/src/sections/Login.jsx
@@ -0,0 +1,9 @@
+const Login = () => {
+    return (
+        <section className="bg-rose-300">
+            <p>HI</p>
+        </section>
+    );
+};
+
+export default Login;
